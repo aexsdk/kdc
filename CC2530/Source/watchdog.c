92b2b54e40cb5347548a3e3f8ada6ffe22041923
杨军 <Tony@eztor.com>
2016-04-13 17:14:09 +0800

    new file:   CC2530/Source/watchdog.c 	new file:   CC2530/Source/watchdog.h

---
+++ CC2530/Source/watchdog.c
@@ -0,0 +1,33 @@
+#include <ioCC2530.h>
+#include "OSAL.h"
+#include "ZGlobals.h"
+#include "OSAL_Nv.h"
+#include "AF.h"
+#include "aps_groups.h"
+#include "ZDApp.h"
+
+#include "watchdog.h"
+
+void Init_Watchdog(void) 
+{ 
+  WDCTL = 0x00;
+  //时间间隔一秒，看门狗模式
+  WDCTL |= 0x08;	//启动看门狗
+}
+
+void SET_MAIN_CLOCK(int source)
+{
+  if(source){
+    CLKCONCMD |= 0x40;/*RC*/
+    while(!(CLKCONSTA&0X40));/*待稳*/
+  }else{
+    CLKCONCMD&=~0x47;/*晶振*/
+    while((CLKCONSTA&0X40));/*待稳*/
+  }
+}
+
+void FeetDog(void)
+{
+  WDCTL = 0xA0 |(WDCTL&0x0F);
+  WDCTL = 0x50 |(WDCTL&0x0F);
+}
---
+++ CC2530/Source/watchdog.h
@@ -0,0 +1,10 @@
+#ifndef __CC2530__WATCHDOG__
+
+#define __CC2530__WATCHDOG__
+
+void Init_Watchdog(void);
+void SET_MAIN_CLOCK(int source);
+void FeetDog(void);
+
+
+#endif
